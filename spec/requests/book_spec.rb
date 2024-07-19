8db8b45f69d0a94e48096a25e83f130fd917a6e7
Jay <jayeshpatil8041@gmail.com>
2024-07-19 17:24:42 +0530

    add spec to test create action

--- spec/requests/book_spec.rb
+++ spec/requests/book_spec.rb
@@ -1,12 +1,25 @@
 require 'rails_helper'
 
 describe 'Books API', type: :request do
-  it 'returns all the books' do
-    FactoryBot.create(:book, title: '1984', author: 'George Orwell')
-    FactoryBot.create(:book, title: 'The Time machine', author: 'H.G. Wells')
-    get '/api/v1/books'
-
-    expect(response).to have_http_status(:success)
-    expect(JSON.parse(response.body).size).to eq(2)
+  describe 'GET /books' do
+    it 'returns all the books' do
+      FactoryBot.create(:book, title: '1984', author: 'George Orwell')
+      FactoryBot.create(:book, title: 'The Time machine', author: 'H.G. Wells')
+      get '/api/v1/books'
+  
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body).size).to eq(2)
+    end
   end
+
+  describe 'POST /books' do
+    it 'creats a new book' do
+      expect {
+          post '/api/v1/books', params: { book: {title: 'The Martian', author: 'Andy Weir'} }
+      }.to change { Book.count }.from(0).to(1)
+
+      expect(response).to have_http_status(:created)
+    end
+  end
+
 end
