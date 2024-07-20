2ceb7e1c1b5b1c562c380e5ce8c81234ac762a34
Jay <jayeshpatil8041@gmail.com>
2024-07-20 18:19:19 +0530

    generate a JWT token and add spec to test it

--- app/services/authentication_token_service.rb
+++ app/services/authentication_token_service.rb
@@ -1,5 +1,9 @@
 class AuthenticationTokenService
+  HMAC_SECRET = 'my$ecretK3y'
+  ALGORITHM_TYPE = 'HS256'
   def self.call
-    '123'
+    payload = { "data" => 'test' }
+
+    JWT.encode payload, HMAC_SECRET, ALGORITHM_TYPE
   end
 end
--- spec/services/authentication_token_service_spec.rb
+++ spec/services/authentication_token_service_spec.rb
@@ -2,8 +2,21 @@ require 'rails_helper'
 
 describe AuthenticationTokenService do
   describe '.call' do
+    let(:token) { described_class.call }
     it 'returns a valid token' do
-      expect(described_class.call).to eq('123')
+      decoded_token = JWT.decode(
+        token,
+        described_class::HMAC_SECRET,
+        true,
+        { algorithm: described_class::ALGORITHM_TYPE }
+      )
+      
+      expect(decoded_token).to eq(
+        [
+          {"data" => "test"},
+          {"alg" => 'HS256'}
+        ]
+      )
     end
   end
 end
