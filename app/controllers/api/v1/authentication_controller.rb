bf9652265939d459bcba210c58fe5d9c35b2520e
Jay <jayeshpatil8041@gmail.com>
2024-07-20 19:14:57 +0530

    add test for unhappy path with password doesn't match

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,12 +1,14 @@
 module Api
   module V1
     class AuthenticationController < ApplicationController
+      class AuthenticationError < StandardError; end
       rescue_from ActionController::ParameterMissing, with: :parameter_missing
+      rescue_from AuthenticationError, with: :handle_authenticated
 
       def create
-        p params.require(:password).inspect
+        user = User.find_by(username: params.require(:username))
 
-        user = User.find_by(username: params[:username])
+        raise AuthenticationError unless user.authenticate(params.require(:password))
         token = AuthenticationTokenService.call(user.id)
 
         render json: { token: token }, status: :created
@@ -16,6 +18,10 @@ module Api
 
       def parameter_missing(e)
         render json: { error: e.message.split("\n").first }, status: :unprocessable_entity
+      end
+
+      def handle_authenticated
+        head :unauthorized
       end
 
     end
--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -2,13 +2,14 @@ require 'rails_helper'
 
 describe 'Authentication', type: :request do
   describe 'POST /authenticate' do
-    let(:user) { FactoryBot.create(:user, username: 'User1') }
+    let(:user) { FactoryBot.create(:user, username: 'user1', password: 'password') }
+
     it 'authenticates the client' do
       post '/api/v1/authenticate', params: { username: user.username, password: 'password' }
 
       expect(response).to have_http_status(:created)
       expect(response_body).to eq(
-        'token' => '123'
+        'token' => AuthenticationTokenService.call(user.id)
       )
     end
 
@@ -28,6 +29,12 @@ describe 'Authentication', type: :request do
       expect(response_body).to eq({
         'error' => 'param is missing or the value is empty: password'
       })
+    end
+
+    it 'return error when password is incorrect' do
+      post '/api/v1/authenticate', params: { username: user.username, password: 'password1' }
+
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 end
